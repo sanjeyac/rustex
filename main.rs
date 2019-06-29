f1dbd673be55aa6f27e956e6e62061abb795d16f
sanjeyac <sanjeyac>
2019-06-29 04:21:27 +0200

    faster solution

--- main.rs
+++ main.rs
@@ -1,6 +1,9 @@
+use std::char;
 
 /**************************************
-*  Simple Version
+ * 
+ *  Simple Version - time 1.731 sec
+ * 
 ***************************************/
 fn rusthero_simple(input: String) -> String {
 
@@ -29,7 +32,9 @@ fn rusthero_simple(input: String) -> String {
 
 
 /**************************************
-*  Accumulated State Version
+ * 
+ *  Accumulated State Version -  time 1.099 sec
+ * 
 ***************************************/
 
 struct AccumulatorState {
@@ -80,15 +85,63 @@ fn rusthero_accumulator_state(input: String) -> String {
 
 
 
+/**************************************
+ * 
+ *  Fast  Version - lot of memory needed BUT TIME 0.307 sec
+ * 
+***************************************/
+fn rusthero_fast(input: String) -> String {
+
+    let mut buffer: [char; 500000] = ['0'; 500000];
+    let mut size = 0;
+
+    let mut output = String::from("");
+    let mut counter = 0;
+    let mut last_char  = '0';
+
+    for c in input.chars() { 
+        if last_char == c || last_char == '0' {
+            counter +=1;
+        } else {
+            buffer[size] = char::from_digit(counter, 10).unwrap();
+            buffer[size+1] = last_char;
+            counter = 1;              
+            size +=2;
+        }
+        last_char = c;
+               
+    }
+    output.push_str(&counter.to_string());
+    output.push_str(&last_char.to_string());  
+    buffer[size] = char::from_digit(counter, 10).unwrap();
+    buffer[size+1] = last_char;
+    counter = 1;  
+    size +=2;
+
+    return buffer[0 .. size].into_iter().collect();
+}
+
 
 fn main() {
 
-    let mut value = String::from("22164224441");
+    let input = String::from("22164224441");
+    let mut value = input.clone();
+
+    let mut max = 0;
 
     for _x in 0..40 {
-        value = rusthero_accumulator_state(value);
+        
+         value = rusthero_fast(value);
+        // value = rusthero_accumulator_state(value);
+        //value = rusthero_simple(value);
+
+        if (max < value.len()){
+            max = value.len();
+        }
     } 
 
-    println!("{}",value.len());
+    println!("max needed buffer size {}",max);
+    println!("final value length: {} of input {}",value.len(),input);
+
 }
 
