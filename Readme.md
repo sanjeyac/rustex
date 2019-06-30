9550ce01002ba81ce6ebbc1bf0236017d028c20f
sanjeyac <sanjeyac>
2019-06-30 19:39:35 +0200

    cleaning solution

--- Readme.md
+++ Readme.md
@@ -3,6 +3,31 @@ First Application in Rust for the RUSTHERO contest
 A first quick prototype have been done in JS in the file prototype/main.js
 
 After that the prototype has been implemented in Rust
-three possible solutions has been benchmarked with the `time` command to check the execution speed
 
-see the comments in main.rs over each function for the results
+```
+fn rusthero_simple(input: String) -> String {
+
+    let mut output = String::from("");
+    let mut counter = 0;
+    let mut last_char  = '0';
+
+    for c in input.chars() { 
+
+        if last_char == c || last_char == '0' {
+            counter +=1;
+        } else {
+            output.push_str(&counter.to_string());
+            output.push_str(&last_char.to_string()); 
+            counter = 1;              
+        }
+        last_char = c;
+    }
+    output.push_str(&counter.to_string());
+    output.push_str(&last_char.to_string());  
+
+    return output;
+
+}
+```
+
+after this a solution using the fold has been implemented
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,28 @@
 use std::char;
 use std::collections::LinkedList;
 
-/**************************************
+
+/**
+ * Accumulator state
  * 
- *  Simple Version 
+ * After each character has been read
+ * if the character is equals to character in the state
+ *      then it will increment the counter,
+ *      else the character will increment the list and the counter will be reset
  * 
- *  time 1.731 sec
- *  
- *  with compiler optimization
- *  time 0.127 sec
- * 
-***************************************/
-fn rusthero_simple(input: String) -> String {
-
-    let mut output = String::from("");
-    let mut counter = 0;
-    let mut last_char  = '0';
-
-    for c in input.chars() { 
-
-        if last_char == c || last_char == '0' {
-            counter +=1;
-        } else {
-            output.push_str(&counter.to_string());
-            output.push_str(&last_char.to_string()); 
-            counter = 1;              
-        }
-        last_char = c;
-    }
-    output.push_str(&counter.to_string());
-    output.push_str(&last_char.to_string());  
-
-    return output;
-
-}
-
-
-
-/**************************************
- * 
- *  Accumulated State Version -  
- * 
- *  time 1.099 sec
- * 
- *  with compiler optimization
- *  time 0.097 sec
- * 
-***************************************/
-
+ * buffer is the final output
+ */
 struct AccumulatorState {
     last_letter: char,
     count: u8,
-    output: String
+    buffer: LinkedList<char> 
 }
 
 
 impl AccumulatorState {
 
     fn new () -> AccumulatorState {
-        return AccumulatorState { last_letter: '0', count: 0, output: String::new() };
+        return AccumulatorState { last_letter: '0', count: 0, buffer: LinkedList::new() };
     }
 
     fn accumulate(mut self, current_letter: &char) -> AccumulatorState {       
@@ -74,14 +38,24 @@ impl AccumulatorState {
     }
 
     fn add_to_output(&mut self) {
-        self.output.push_str(&self.count.to_string());
-        self.output.push(self.last_letter);         
+        let count_as_char = char::from_digit(self.count.into(), 10).unwrap();
+        self.buffer.push_back(count_as_char);
+        self.buffer.push_back(self.last_letter);     
+    }
+
+    fn as_string(&mut self) -> String {
+        let final_string = self.buffer.clone().into_iter().collect();
+        return final_string;
     }
 
 }
 
 
-fn rusthero_accumulator_state(input: String) -> String {
+
+/**
+ * 
+ */
+fn rusthero(input: String) -> String {
 
     let empty_accumulator = AccumulatorState::new();
 
@@ -91,78 +65,19 @@ fn rusthero_accumulator_state(input: String) -> String {
 
     result.add_to_output();
 
-    return result.output;
+    return result.as_string();
 }
 
 
-
-/**************************************
- * 
- *  BEST SOLUTION SO FAR
- * 
- *  Fast  Version - 
- * 
- *  time 0,407
- *  
- *  with compiler optimazation
- *  time 0.087 sec
- * 
- * 
- * we could use an array instead of a list to improve performance
- * but then we need to manage array re-allocation
- * when the capacity of the array is close to the end
- * 
-***************************************/
-fn rusthero_fast(input: String) -> String {
-
-
-    let mut buffer: LinkedList<char> = LinkedList::new(); // append with O(1)
-    let mut size = 0;
-
-    let mut output = String::from("");
-    let mut counter = 0;
-    let mut last_char  = '0';
-
-    for c in input.chars() { 
-        if last_char == c || last_char == '0' {
-            counter +=1;
-        } else {
-            buffer.push_back(char::from_digit(counter, 10).unwrap());
-            buffer.push_back(last_char);
-            counter = 1;              
-
-        }
-        last_char = c;
-               
-    }
-    buffer.push_back(char::from_digit(counter, 10).unwrap());
-    buffer.push_back(last_char);
-
-    return buffer.into_iter().collect();
-}
-
-// rustc  main.rs
-fn main() {
-
-    let input = String::from("22164224441");
-    let mut value = input.clone();
-
-    let mut max = 0;
-
-    for _x in 0..40 {
-        
-         value = rusthero_fast(value);
-        // value = rusthero_accumulator_state(value);
-        // value = rusthero_simple(value);
-
-        if (max < value.len()){
-            max = value.len();
-        }
-    } 
-
-    println!("max needed buffer size {}",max);
-    println!("final value length: {} of input {}",value.len(),input);
-
+/**
+ * MAIN
+ */
+fn main() {    
+    let mut value =  String::from("22164224441");
+    for _x in 0..40 {        
+         value = rusthero(value);
+    }    
+    println!("final value length: {}",value.len());
 }
 
 
@@ -173,38 +88,38 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let result = rusthero_fast("1".to_string());
+        let result = rusthero("1".to_string());
         assert_eq!( result , "11" );
     }
 
     #[test]
     fn test_11() {
-        let result = rusthero_fast("11".to_string());
+        let result = rusthero("11".to_string());
         assert_eq!( result , "21" );
     }
 
     #[test]
     fn test_31() {
-        let result = rusthero_fast("31".to_string());
+        let result = rusthero("31".to_string());
         assert_eq!( result , "1311" );
     }
 
     #[test]
     fn test_3211() {
-        let result = rusthero_fast("3211".to_string());
+        let result = rusthero("3211".to_string());
         assert_eq!( result , "131221" );
     }
 
     #[test]
     fn test_111223() {
-        let result = rusthero_fast("111223".to_string());
+        let result = rusthero("111223".to_string());
         assert_eq!( result , "312213" );
     }        
 
 
     #[test]
     fn test_312213() {
-        let result = rusthero_fast("312213".to_string());
+        let result = rusthero("312213".to_string());
         assert_eq!( result , "1311221113" );
     }        
 }
